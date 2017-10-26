e8b8159eaa8f5afea46deca9e6f7114047d2162e
Simona Stoyanova <sstoyanova26@gmail.com>
2017-10-26 17:49:34 +0300

    Added license to java files Fixed cover_image_view size in activity_animated_fab.xml

--- library/src/main/java/bg/devlabs/flyingfab/AppBarStateChangeListener.kt
+++ library/src/main/java/bg/devlabs/flyingfab/AppBarStateChangeListener.kt
@@ -6,6 +6,25 @@ import bg.devlabs.flyingfab.AppBarStateChangeListener.AppBarState
 
 
 /**
+ * Copyright (c) 2017 Simona Stoyanova
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  * Created by Simona Stoyanova on 10/19/17.
  * simona@devlabs.bg
  * A listener which is called when the appbar state changes
@@ -13,7 +32,6 @@ import bg.devlabs.flyingfab.AppBarStateChangeListener.AppBarState
  *
  * @see AppBarState
  */
-
 abstract class AppBarStateChangeListener : AppBarLayout.OnOffsetChangedListener {
     private var isButtonTop = true
 
